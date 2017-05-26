4856b1b13b5f0c6972a5e8b96dd1b9dbb10640b8
harishkumar <haris.kumar4095@gmail.com>
2017-05-25 19:24:37 -0500

    Add files via upload

---
+++ nonLengthy/src/nonLengthy1/extendedclass.java
@@ -0,0 +1,18 @@
+package nonLengthy1;
+
+public class extendedclass {
+	
+	
+	public void add()
+	
+	{
+		System.out.println("i am sending this class to main method");
+		
+	}
+	
+     public String sub(){
+    	 System.out.println("this is return type. and i am returning to main");
+    	 return "returned";
+     }
+     
+}
---
+++ nonLengthy/src/nonLengthy1/mainclass.java
@@ -0,0 +1,15 @@
+package nonLengthy1;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		extendedclass d= new extendedclass();
+		d.add();
+		d.sub();
+		System.out.println(d.sub());
+
+	}
+
+}
